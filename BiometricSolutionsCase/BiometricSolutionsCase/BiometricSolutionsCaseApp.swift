8f1439395e9de3bfc4c42176bc819cdd42f9a3f1
Simon Elhoej Steinmejer <simonelhoej@gmail.com>
2025-06-23 08:24:00 +0200

    Initial commit

---
+++ BiometricSolutionsCase/BiometricSolutionsCase/BiometricSolutionsCaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiometricSolutionsCaseApp.swift
+//  BiometricSolutionsCase
+//
+//  Created by Simon Elhøj Steinmejer on 23/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct BiometricSolutionsCaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
